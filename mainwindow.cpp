0a688744b287edc4443b619b1cb8cd1634e7c105
vigorweijia <vigorweijia@gmail.com>
2019-10-20 23:33:18 +0800

    drawing line ...

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -104,9 +104,47 @@ void MainWindow::onReceive_DrawLine(int id, float x0, float y0, float x1, float 
 }
 
 void MainWindow::drawLineDDA(float x0, float y0, float x1, float y1) {
-
+    if(x1 - x0 == 0) {QMessageBox::warning(this, "error", tr("vertical!"));return;}
+    float m = (y1 - y0)/(x1 - x0);
+    if(x1 < x0) {swap(x0,x1); swap(y0,y1);}
+    if(abs(m) <= 1) {
+        qPainter->drawPoint((int)x0, (int)y0);
+        int yi = (int)y0;
+        for(int xi = (int)x0 + 1; xi <= (int)x1; xi++) {
+            yi = (int)((float)yi + m);
+            qPainter->drawPoint(xi, yi);
+        }
+    }
+    else {
+        qPainter->drawPoint((int)x0, (int)y0);
+        int xi = (int)x0;
+        for(int yi = (int)y0 + 1; yi <= (int)y1; yi++) {
+            xi = (int)((float)xi + (1/m));
+            qPainter->drawPoint(xi, yi);
+        }
+    }
 }
 
 void MainWindow::drawLineBresenham(float x0, float y0, float x1, float y1) {
+    if(x1 < x0) {swap(x0,x1); swap(y0,y1);}
+    if(x1 == x0) {
+        if(y1 < y0) swap(y1, y0);
+        for(int yi = (int)y0; yi <= (int)y1; yi++) qPainter->drawPoint((int)x0, yi);
+        return;
+    }
+    if(y1 == y0) {
+        for(int xi = (int)x0; xi <= (int)x1; xi++) qPainter->drawPoint(xi, (int)y0);
+        return;
+    }
+    if(x1 - x0 == y1 - y0) {
+        for(int xi = (int)x0; xi <= (int)x1; xi++) qPainter->drawPoint(xi, (int)y0 + xi - (int)x0);
+        return;
+    }
+    float m = (y1 - y0)/(x1 - x0);
+    if(abs(m) < 1) {
 
+    }
+    else {
+
+    }
 }
