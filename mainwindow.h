b4476600bf776739ef10d21ef34e3cd1a3e23399
vigorweijia <vigorweijia@gmail.com>
2019-10-31 10:37:52 +0800

    加了注释

--- mainwindow.h
+++ mainwindow.h
@@ -41,20 +41,26 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+    //-----width and height of canvas-----
     int newCanvasWidth;
     int newCanvasHeight;
+    //------------------------------------
+
+    //--------kinds of dialogs------------
     NewCanvasDialog *newCanvasDialog;
     LineDialog *lineDialog;
     EllipseDialog *ellipseDialog;
     PolygonDialog *polygonDialog;
     ColorDialog *colorDialog;
+    //------------------------------------
+
     //---------Drag mouse to draw---------
     int selectedDrawEvent;
     int clickTimes;
     int selectedX0,selectedY0,selectedX1,selectedY1;
     //------------------------------------
 
-
+    //------structure of Graphic Unit-----
     struct GraphicUnit {
         int id;
         int type;
@@ -66,31 +72,44 @@ public:
         }color;
     };
     vector<GraphicUnit> v;
+    //------------------------------------
 
 protected:
-    int baseX,baseY; //用于计算相对于label的坐标
+    //----------mouse drag event--------------
+    int baseX,baseY; //用于计算相对于label(Canvas)的坐标
     void mousePressEvent(QMouseEvent *e);
     void mouseMoveEvent(QMouseEvent *e);
     void mouseReleaseEvent(QMouseEvent *e);
+    //----------------------------------------
 
 private:
     Ui::MainWindow *ui;
+    //-------主画笔和画布（置底）--------
     QPainter *qPainter;
     QPixmap *qPixmap;
+    //--------------------------------
+
+    //------临时画笔和画布（置顶）-------
     QPainter *tempPainter;
     QPixmap *tempPixmap;
-    QColor *qColor;
+    //--------------------------------
+
+    QColor *qColor;//color of painter
+
+    //----------各类事件的处理函数-------------------
     void createNewCanvas(int width, int height);
-    bool isIdExist(int id);
+    bool isIdExist(int id); //判断图元id是否存在
     void drawLineDDA(float x0, float y0, float x1, float y1, QPainter *thisPainter);
     void drawLineBresenham(float x0, float y0, float x1, float y1, QPainter *thisPainter);
     void drawEllipse(int x, int y, int rx, int ry, QPainter *thisPainter);
     void drawPolygon(int n, vector<int> v, int type, QPainter *thisPainter);
     void setColor(int R, int G, int B, QPainter *thisPainter);
-    void createTempPixmapExceptId(int id);
+    void createTempPixmapExceptId(int id); //绘制除id以外的所有图元
+    //--------------------------------------------
 
 private slots:
-    //--------------File----------------------
+
+    //--------------File栏--------------------
     void on_actionNewCanvas_triggered();
     //void on_actionOpen_triggered();
     //void on_actionSave_triggered();
@@ -98,7 +117,7 @@ private slots:
     void on_actionImportFromFile_triggered();
     //----------------------------------------
 
-    //--------------Edit----------------------
+    //--------------Edit栏--------------------
     void on_actionResetCanvas_triggered();
     void on_actionColor_triggered();
     void on_actionLine_triggered();
@@ -111,7 +130,7 @@ private slots:
     void on_actionEllipseIcon_triggered();
     //----------------------------------------
 
-    //--------------------------From child dialog--------------------------------------
+    //-------------------------Signals from child dialog-------------------------------
     void onReceive_NewCanvasDialogAcceptedEvent(int width, int height);
     void onReceive_DrawLine(int id, float x0, float y0, float x1, float y1, int type);
     void onReceive_DrawEllipse(int id, int x, int y, int rx, int ry);
